50945adf1f2faf313cbb0d17e219ae720897ff50
Callum Inglis <callum.inglis@4oh4.co.uk>
2024-06-19 09:40:13 +0100

    Form Creation; Event Handlers

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -8,12 +8,40 @@ const Bookshelf = () => {
 
     const [newBook, setNewBook] = useState({ title: '', author: '' });
 
+    const handleInputChange = (e) => {
+        const book = {...newBook};
+        book.title = e.target.form.elements.title.value;
+        book.author = e.target.form.elements.author.value;
+
+        setNewBook(book);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setBooks([...books, newBook]);
+        setNewBook({ title: '', author: '' });
+    }
       
     return (
         <div className="bookshelfDiv">
             <div className="formDiv">
                 <h3>Add a Book</h3>
                 {/* Form will go here */}
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Title:
+                        <input type="text" value={newBook.title} name="title" onChange={handleInputChange} />
+                    </label>
+
+                    <label>
+                        Author:
+                        <input type="text" value={newBook.author} name="author" onChange={handleInputChange} />
+                    </label>
+                    
+                    <button type="submit">
+                        Add Book
+                    </button>
+                </form>
             </div>
             <div className="bookCardsDiv">{/* Book cards will display here */}</div>
         </div>
